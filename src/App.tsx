f2c23a2c2021a86aec2c779d403850599b4ef9f9
Rafael Heredia Bastidas <heredia.developer@gmail.com>
2020-01-22 19:44:09 -0300

    added antd ui framework

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,272 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Select, TreeSelect, Typography} from "antd";
+import {IBreedResponse} from "./services/IBreedResponse";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const {Title} = Typography;
+const {Option} = Select;
+const {SHOW_PARENT} = TreeSelect;
+
+const breeds: IBreedResponse = {
+    "message": {
+        "affenpinscher": [],
+        "african": [],
+        "airedale": [],
+        "akita": [],
+        "appenzeller": [],
+        "australian": [
+            "shepherd"
+        ],
+        "basenji": [],
+        "beagle": [],
+        "bluetick": [],
+        "borzoi": [],
+        "bouvier": [],
+        "boxer": [],
+        "brabancon": [],
+        "briard": [],
+        "buhund": [
+            "norwegian"
+        ],
+        "bulldog": [
+            "boston",
+            "english",
+            "french"
+        ],
+        "bullterrier": [
+            "staffordshire"
+        ],
+        "cairn": [],
+        "cattledog": [
+            "australian"
+        ],
+        "chihuahua": [],
+        "chow": [],
+        "clumber": [],
+        "cockapoo": [],
+        "collie": [
+            "border"
+        ],
+        "coonhound": [],
+        "corgi": [
+            "cardigan"
+        ],
+        "cotondetulear": [],
+        "dachshund": [],
+        "dalmatian": [],
+        "dane": [
+            "great"
+        ],
+        "deerhound": [
+            "scottish"
+        ],
+        "dhole": [],
+        "dingo": [],
+        "doberman": [],
+        "elkhound": [
+            "norwegian"
+        ],
+        "entlebucher": [],
+        "eskimo": [],
+        "frise": [
+            "bichon"
+        ],
+        "germanshepherd": [],
+        "greyhound": [
+            "italian"
+        ],
+        "groenendael": [],
+        "hound": [
+            "afghan",
+            "basset",
+            "blood",
+            "english",
+            "ibizan",
+            "walker"
+        ],
+        "husky": [],
+        "keeshond": [],
+        "kelpie": [],
+        "komondor": [],
+        "kuvasz": [],
+        "labrador": [],
+        "leonberg": [],
+        "lhasa": [],
+        "malamute": [],
+        "malinois": [],
+        "maltese": [],
+        "mastiff": [
+            "bull",
+            "english",
+            "tibetan"
+        ],
+        "mexicanhairless": [],
+        "mix": [],
+        "mountain": [
+            "bernese",
+            "swiss"
+        ],
+        "newfoundland": [],
+        "otterhound": [],
+        "papillon": [],
+        "pekinese": [],
+        "pembroke": [],
+        "pinscher": [
+            "miniature"
+        ],
+        "pitbull": [],
+        "pointer": [
+            "german",
+            "germanlonghair"
+        ],
+        "pomeranian": [],
+        "poodle": [
+            "miniature",
+            "standard",
+            "toy"
+        ],
+        "pug": [],
+        "puggle": [],
+        "pyrenees": [],
+        "redbone": [],
+        "retriever": [
+            "chesapeake",
+            "curly",
+            "flatcoated",
+            "golden"
+        ],
+        "ridgeback": [
+            "rhodesian"
+        ],
+        "rottweiler": [],
+        "saluki": [],
+        "samoyed": [],
+        "schipperke": [],
+        "schnauzer": [
+            "giant",
+            "miniature"
+        ],
+        "setter": [
+            "english",
+            "gordon",
+            "irish"
+        ],
+        "sheepdog": [
+            "english",
+            "shetland"
+        ],
+        "shiba": [],
+        "shihtzu": [],
+        "spaniel": [
+            "blenheim",
+            "brittany",
+            "cocker",
+            "irish",
+            "japanese",
+            "sussex",
+            "welsh"
+        ],
+        "springer": [
+            "english"
+        ],
+        "stbernard": [],
+        "terrier": [
+            "american",
+            "australian",
+            "bedlington",
+            "border",
+            "dandie",
+            "fox",
+            "irish",
+            "kerryblue",
+            "lakeland",
+            "norfolk",
+            "norwich",
+            "patterdale",
+            "russell",
+            "scottish",
+            "sealyham",
+            "silky",
+            "tibetan",
+            "toy",
+            "westhighland",
+            "wheaten",
+            "yorkshire"
+        ],
+        "vizsla": [],
+        "waterdog": [
+            "spanish"
+        ],
+        "weimaraner": [],
+        "whippet": [],
+        "wolfhound": [
+            "irish"
+        ]
+    },
+    "status": "success"
 };
+
+const breedAndSubBreeds = Object.keys(breeds.message).filter(breed => breeds.message[breed].length > 0).map((breed) => {
+    return {breed: breed, subBreeds: breeds.message[breed]}
+});
+
+const treeData = breedAndSubBreeds.map((breedAndSubBreed, index) => {
+    const value = "0-" + index;
+    return {
+        title: breedAndSubBreed.breed,
+        value: value,
+        key: value,
+        children: breedAndSubBreed.subBreeds.map((subBreed, subIndex) => {
+            return {
+                title: subBreed,
+                value: value + "-" + subIndex,
+                key: value + "-" + subIndex
+            };
+        })
+    };
+});
+
+class App extends React.Component {
+    state = {
+        value: ['0-0-0'],
+    };
+
+    onChange = (value: any) => {
+        console.log('onChange ', value);
+        this.setState({value});
+    };
+
+    handleChangeBreed = (value: any) => {
+        console.log(`selected ${value}`);
+    };
+
+    render() {
+        const tProps = {
+            treeData,
+            value: this.state.value,
+            onChange: this.onChange,
+            treeCheckable: true,
+            showCheckedStrategy: SHOW_PARENT,
+            searchPlaceholder: 'Please select',
+            style: {
+                width: '100%',
+            },
+        };
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <Title>h1. Ant Design</Title>
+                    <Select mode="tags" style={{width: '100%'}} placeholder="Tags Mode"
+                            onChange={this.handleChangeBreed}>
+                        {Object.keys(breeds.message).map(breed => (
+                            <Option key={breed}>{breed}</Option>))}
+                    </Select>
+
+                    <TreeSelect {...tProps} />
+                </header>
+            </div>
+        );
+    }
+}
 
 export default App;
